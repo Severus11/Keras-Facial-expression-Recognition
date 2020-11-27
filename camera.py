c4186d7f3f9c032d795b854b564d437d76d6ddbb
Severus11 <parthsgupta@gmail.com>
2020-11-27 15:06:52 +0530

    updated code

--- camera.py
+++ camera.py
@@ -8,7 +8,7 @@ font = cv2.FONT_HERSHEY_SIMPLEX
 
 class VideoCamera(object):
     def __init__(self):
-        self.video = cv2.VideoCapture(0)
+        self.video = cv2.VideoCapture("facial_exp.mkv")
 
     def __del__(self):
         self.video.release()
