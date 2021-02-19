2239cf0d1930ad74503a90798843b07ee9800170
Severus11 <parthsgupta@gmail.com>
2021-02-20 00:48:44 +0530

    started frontend

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, Response
 from camera import VideoCamera
 
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder='templates',static_folder='CSS')
 
 @app.route('/')
 def index():
